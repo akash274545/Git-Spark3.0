bb6aedf533260b9ca561cbd4188fa5931fe2c2eb
Akash Narayankar <akashnarayankar2745@gmail.com>
2025-03-19 12:17:57 +0530

    Hello added

--- Program.java
+++ Program.java
@@ -2,6 +2,10 @@ class Program
 { 
       public static void main(String[] args) {
     
-        System.out.println("From Program");
+        System.out.println("From Program"); 
+}
+public void show()
+{
+    System.out.println("Heloo");
 }
 }
